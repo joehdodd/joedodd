8fc89d6352fe7eafc11c67acbf10e364edaa52a2
Joe Dodd <JoeDodd@Joes-MacBook-Pro.local>
2019-02-17 22:09:02 -0500

    copy tweaks

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const USER = 'joehdodd';
 const getContent = async function(e) {
+  const USER = 'joehdodd';
   const reposContainer = document.getElementById('reposContainer');
-  const repos = await fetch('https://api.github.com/users/joehdodd/repos').then(res => res.json());
+  const repos = await fetch(`https://api.github.com/users/${USER}/repos`).then(res => res.json());
   const recentlyUpdated = update => {
     const currentDate = new Date();
     const updatedTime = new Date(update).getTime();
     return updatedTime >= (currentDate.setDate(currentDate.getDate() - 7))
   };
-  repos.forEach(repo => {
-
+  return repos.forEach(repo => {
     if (!repo.fork && recentlyUpdated(repo.updated_at)) {
-    // if (true) {
       const repoEl = document.createElement('div');
       repoEl.setAttribute('class', 'repoContainer');
       repoEl.setAttribute('id', `repo_${repo.id}`);
